8f1588626968d4a88e7746fe197a4bfd89651cd6
Daniel Dilts <diltsman@gmail.com>
2025-02-13 19:54:55 -0700

    co_return works

--- src/libexpectedcoroutine/libexpectedcoroutine.cpp
+++ src/libexpectedcoroutine/libexpectedcoroutine.cpp
@@ -1,2 +1,39 @@
+module;
+
+#include <coroutine>
+#include <expected>
+#include <system_error>
+
 export module libexpectedcoroutine;
-export auto my_inc(int const val) -> int { return val + 1; }
+
+export {
+  namespace exco {
+  template <typename T> using result_t = std::expected<T, std::error_code>;
+
+  template <typename T> inline auto unerr(T const t) {
+    return std::unexpected{make_error_code(t)};
+  }
+
+  template <typename T> struct expected_wrapper {
+    result_t<T> &m_result;
+    operator result_t<T>() { return std::move(m_result); };
+  };
+  } // namespace exco
+
+  namespace std {
+  template <typename T, typename... Args>
+  struct coroutine_traits<exco::result_t<T>, Args...> {
+    struct promise_type {
+      exco::result_t<T> m_result{1};
+      auto initial_suspend() noexcept -> std::suspend_never { return {}; }
+      auto final_suspend() noexcept -> std::suspend_never { return {}; }
+      auto return_value(std::error_code ec) { m_result = std::unexpected{ec}; }
+      template <typename T1> auto return_value(T1 &&t) {
+        m_result = std::move(t);
+      }
+      auto get_return_object() { return exco::expected_wrapper<T>{m_result}; }
+      auto unhandled_exception() {}
+    };
+  };
+  } // namespace std
+}
--- test/libexpectedcoroutinetest/libexpectedcoroutinetest.cpp
+++ test/libexpectedcoroutinetest/libexpectedcoroutinetest.cpp
@@ -1,5 +1,13 @@
 import libexpectedcoroutine;
 
 #include <catch2/catch_all.hpp>
+#include <coroutine>
 
-TEST_CASE("Test 1", "") { REQUIRE(my_inc(1) == 2); }
+TEST_CASE("co_return returns expected value", "[expectedcoroutine]") {
+  REQUIRE([]() -> exco::result_t<int> { co_return 1; }() == 1);
+  REQUIRE([]() -> exco::result_t<float> { co_return -1.5; }() == -1.5f);
+  REQUIRE([]() -> exco::result_t<int> {
+    co_return exco::unerr(std::errc::address_in_use);
+  }()
+                          .error() == std::errc::address_in_use);
+}
