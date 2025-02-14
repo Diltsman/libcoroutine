1366a878a10772736cbf5c6c543c0829d11f8c2e
Daniel Dilts <diltsman@gmail.com>
2025-02-13 21:37:41 -0700

    co_await returns errors and values correctly

--- src/libexpectedcoroutine/libexpectedcoroutine.cpp
+++ src/libexpectedcoroutine/libexpectedcoroutine.cpp
@@ -10,7 +10,7 @@ export {
   namespace exco {
   template <typename T> using result_t = std::expected<T, std::error_code>;
 
-  template <typename T> inline auto unerr(T const t) {
+  inline auto unerr(auto const t) {
     return std::unexpected{make_error_code(t)};
   }
 
@@ -23,16 +23,38 @@ export {
   namespace std {
   template <typename T, typename... Args>
   struct coroutine_traits<exco::result_t<T>, Args...> {
+    struct promise_type;
+    template <typename T1> struct awaiter_type {
+      exco::result_t<T1> &m_result;
+      explicit awaiter_type(exco::result_t<T1> &result) noexcept
+          : m_result{result} {}
+      auto await_ready() { return m_result.has_value(); }
+      auto await_suspend(std::coroutine_handle<promise_type> h) {
+        // This should only happen when await_ready() returns false,
+        // which means that has_value() returned false.
+        h.destroy();
+      }
+      auto await_resume() {
+        // This should only happen when await_ready() returns true,
+        // which means that has_value() returned true.
+        return m_result.value();
+      }
+    };
+
     struct promise_type {
-      exco::result_t<T> m_result{1};
+      // Initialize as errno 0 so there are no restrictions on T
+      // caused by initializing this
+      exco::result_t<T> m_result{exco::unerr(static_cast<std::errc>(0))};
       auto initial_suspend() noexcept -> std::suspend_never { return {}; }
       auto final_suspend() noexcept -> std::suspend_never { return {}; }
       auto return_value(std::error_code ec) { m_result = std::unexpected{ec}; }
-      template <typename T1> auto return_value(T1 &&t) {
-        m_result = std::move(t);
-      }
+      auto return_value(auto &&t) { m_result = std::move(t); }
       auto get_return_object() { return exco::expected_wrapper<T>{m_result}; }
       auto unhandled_exception() {}
+      template <typename T1> auto await_transform(exco::result_t<T1> value) {
+        m_result = value;
+        return awaiter_type<T1>{value};
+      }
     };
   };
   } // namespace std
--- test/libexpectedcoroutinetest/libexpectedcoroutinetest.cpp
+++ test/libexpectedcoroutinetest/libexpectedcoroutinetest.cpp
@@ -11,3 +11,39 @@ TEST_CASE("co_return returns expected value", "[expectedcoroutine]") {
   }()
                           .error() == std::errc::address_in_use);
 }
+
+TEST_CASE("co_await continues execution", "[expectedcoroutine]") {
+  REQUIRE([]() -> exco::result_t<int> {
+    int x = co_await []() -> exco::result_t<int> { return 1; }();
+    co_return x + 1;
+  }()
+                          .value() == 2);
+  REQUIRE([]() -> exco::result_t<int> {
+    int x = co_await []() -> exco::result_t<int> { return 1; }();
+    int y = co_await [](int a) -> exco::result_t<int> { return a + 1; }(x);
+    co_return y + 1;
+  }()
+                          .value() == 3);
+}
+
+TEST_CASE("co_await return std::error_code", "[expectedcoroutine]") {
+  REQUIRE([]() -> exco::result_t<int> {
+    int x = co_await []() -> exco::result_t<int> {
+      return exco::unerr(std::errc::address_in_use);
+    }();
+    REQUIRE(false);
+    co_return x + 1;
+  }()
+                          .error() == std::errc::address_in_use);
+  REQUIRE([]() -> exco::result_t<int> {
+    int x = co_await []() -> exco::result_t<int> { return 1; }();
+    REQUIRE(x == 1);
+    int y = co_await [](int i) -> exco::result_t<int> {
+      return exco::unerr(std::errc::address_in_use);
+    }(x);
+    REQUIRE(false);
+    int z = co_await [](int i) -> exco::result_t<int> { return i + 1; }(y);
+    co_return z + 1;
+  }()
+                          .error() == std::errc::address_in_use);
+}
